54fbfe5902ff4739bf3a29424f96b6f4b11af3df
Campus FP <usuarioTom@gmail.com>
2024-02-26 13:38:02 +0100

    Primer commit

---
+++ src/app/tab4/tab4.page.spec.ts
@@ -0,0 +1,11 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Tab4Page } from './tab4.page';
+
+describe('Tab4Page', () => {
+  let component: Tab4Page;
+  let fixture: ComponentFixture<Tab4Page>;
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
